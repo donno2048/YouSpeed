bc87854fcaab0053a782102467b392f14cd37cab
donno2048 <just4now666666@gmail.com>
2021-03-08 10:25:16 +0200

    Update

--- background.js
+++
@@ -1,9 +0,0 @@
-var head = document.getElementsByTagName('head')[0]
-var script = document.createElement("script")
-chrome.storage.local.get(["value"], function (items) {
-    var playbackRate = items.value
-    if (typeof playbackRate === "undefined") {playbackRate = 1}
-    script.appendChild(document.createTextNode("document.getElementsByTagName(\"video\")[0].playbackRate = " + playbackRate))
-    head.appendChild(script)
-    head.removeChild(script)
-})
--- update.js
+++ update.js
@@ -1,7 +1,1 @@
-document.getElementById("update").addEventListener("click", function() {
-    chrome.storage.local.set({value: document.getElementById("val").value}, function() {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.reload(tabs[0].id, {}, ()=>{})
-        })
-    })
-})
+document.getElementById("update").addEventListener("click", function() {chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {chrome.tabs.executeScript(tabs[0].id, {code: `document.getElementsByTagName("video")[0].playbackRate = ${document.getElementById("val").value}`})})})
